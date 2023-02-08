283f3f9df1d1372fb13b8571a0983c57f09c4276
Your name <you@example.com>
2023-02-08 16:58:18 +0100

    debut acceuil css

---
+++ Pages/Accueil.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../src/css/acceuil.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="body_acceuil">
+        <div class="acceuil_start">
+            <p>En savoir plus ?</p>
+            <button>⬇️</button>
+        </div>
+
+        <div class="acceuil_presentation">
+            <div>
+                <p>Tout le monde souhaite voyager, mais pour aller ou, quand, comment,
+                    à quoi s'attendre et que faire sur place,
+                </p>
+                <p>
+                Autant de questions qui nous éloigne de nos reves, ici les 
+                ennuis siparaissent et des amitiés naissent !
+                </p>
+            </div>
+            <div>
+                <img src="../ressources/img/acceuil/presentation1.jpg" alt="" class="presentation1">
+                <img src="../ressources/img/acceuil/presentation2.webp" alt="" class="presentation2">
+                <img src="../ressources/img/acceuil/presentation3.webp" alt="" class="presentation3">
+            </div>
+
+        
+    </div>
+</body>
+</html>
