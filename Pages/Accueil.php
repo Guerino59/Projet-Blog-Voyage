bc75c142a18eebaf90cd0b8bfb9ab8f564f8c233
Your Name <you@example.com>
2023-02-17 12:00:25 +0100

    fichier variable css + footer

--- Pages/Accueil.php
+++ Pages/Accueil.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,10 +8,12 @@
     <title>Acceuil</title>
     <link rel="stylesheet" href="../src/css/acceuil.css">
     <link rel="stylesheet" href="../template/Inscription/sources/style.css">
+    <link rel="stylesheet" href="/Projet-Blog-Voyage/template/footer/sources/footer.css">
     <script src="../src/js/acceuil.js" defer></script>
     <script src="https://kit.fontawesome.com/6e2bb0e8df.js" crossorigin="anonymous"></script>
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
+
 <body>
 
     <div class="body_acceuil">
@@ -25,8 +28,8 @@
                     à quoi s'attendre et que faire sur place,
                 </p>
                 <p>
-                Autant de questions qui nous éloigne de nos reves, ici les 
-                ennuis siparaissent et des amitiés naissent !
+                    Autant de questions qui nous éloigne de nos reves, ici les
+                    ennuis siparaissent et des amitiés naissent !
                 </p>
             </div>
             <div>
@@ -34,40 +37,39 @@
                 <img src="../ressources/img/acceuil/presentation2.webp" alt="" class="presentation2">
                 <img src="../ressources/img/acceuil/presentation3.webp" alt="" class="presentation3">
             </div>
-  
+
         </div>
 
         <div class="acceuil_formulaire">
             <div class="passport">
                 <!-- choisir design -->
-                    <div class="img_passport">
+                <div class="img_passport">
                     <div class="passport_write">
-                            <p class="name"></p>
-                            <p class="mail"></p>
-                            <p class="dateOfBirth"></p>
-                            <p class="country"></p>
-                            <p class="signature"></p>
-                    </div> 
-            </div>
+                        <p class="name"></p>
+                        <p class="mail"></p>
+                        <p class="dateOfBirth"></p>
+                        <p class="country"></p>
+                        <p class="signature"></p>
+                    </div>
+                </div>
             </div>
             <div class="form_passport">
                 <div>
-                <!-- Intégrer le formulaire -->
-                <?php require "../template/Inscription/_formInscription.php" ?>
+                    <!-- Intégrer le formulaire -->
+                    <?php require "../template/Inscription/_formInscription.php" ?>
                 </div>
             </div>
         </div>
 
         <div class="acceuil_bas_de_page">
             <div class="acceuil_bas_de_page_info">
-                    <p>En savoir plus sur nous ?</p>
-                    <p>(faire un petit résumer pour parler de la création du site)</p>
+                <p>En savoir plus sur nous ?</p>
+                <p>(faire un petit résumer pour parler de la création du site)</p>
             </div>
         </div>
 
-        <footer>
-            <p>Nos divers liens et reseaux</p>
-        </footer>
+        <?php require "../template/footer/_footer.php" ?>
     </div>
 </body>
+
 </html>
